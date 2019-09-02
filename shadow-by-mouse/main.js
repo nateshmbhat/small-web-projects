b4a75babbe0fd7a8e7753ed49ba12c9d3240e16e
nateshmbhat <nateshmbhat1@gmail.com>
2019-09-02 20:30:01 +0530

    refactor

--- shadow-by-mouse/main.js
+++ shadow-by-mouse/main.js
@@ -1,21 +1,10 @@
-const name = document.getElementById('name');
-const { x, y, width, depth } = name.getBoundingClientRect();
-const namePos = {x:x , y:y} ;
-const mousePos = {x:1 , y:1}
-const xoffSetAdjust = 500; 
-const yoffSetAdjust = 170 ; 
+const name = document.getElementById('name')
+const {x ,y , height , width} = name.getBoundingClientRect() 
 
-document.addEventListener('mousemove', (obj) => {
-    mousePos.x = obj.screenX;
-    mousePos.y = obj.screenY;
-
-    const res = {x : namePos.x-mousePos.x , y : namePos.y-mousePos.y } ; 
-    let xShadow = (res.x+xoffSetAdjust) , yShadow = (res.y+yoffSetAdjust) ;
-    let xSign = xShadow/Math.abs(xShadow) , ySign = yShadow/Math.abs(yShadow)
-    let xVal= Math.abs(xShadow) , yVal = Math.abs(yShadow) ; 
-    
-    name.style.textShadow = `${xSign==-1?'-':''}${Math.min(xVal,50)}px ${ySign==-1?'-':''}${Math.min(yVal,22)}px ${Math.max( (Math.max(xVal,yVal)-20)*0.1, 8)}px black` ;
-    console.log( xSign , ySign ,  name.style.textShadow) 
+document.addEventListener('mousemove' , (info)=>{
+    const mouseX = info.clientX , mouseY= info.clientY ; 
+    const shadow = { x : x-mouseX + width/2  , y :y-mouseY + height/2 } ; 
+    console.log(shadow)
+    const xval = Math.abs(shadow.x) , yval = Math.abs(shadow.y) ; 
+    name.style.textShadow = `${shadow.x}px ${shadow.y}px ${Math.max(Math.max(xval,yval)-30 , 6)}px`
 })
-
-
